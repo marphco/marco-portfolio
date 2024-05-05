0a364e58dd773935c2bdb5e2b201002d3bf0f25b
marphco <5993166+marphco@users.noreply.github.com>
2024-05-05 17:57:59 -0400

    app ready to be deployed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Hero, About, Portfolio, PortfolioNavbar, Contact, Resume } from './components'
+
+function App() {
+  return (
+    <div className="App">
+      <PortfolioNavbar />
+      <Hero />
+      <About/>
+      <Portfolio/>
+      <Contact/>
+      <Resume/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/resume.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Container, Row, Button } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import resume from './assets/documents/Resume.pdf';
+
+import {
+    Resume2,
+    Github,
+    Linkedin,
+  } from "./assets/img";
+  
+
+const Resume = () => {
+
+    const handleDownloadResume = () => {
+        window.open(resume);
+    };
+
+
+    return(
+        <Row id="resume">
+            <Container className="resume-block d-flex justify-content-center w-100">
+            <img className="resume-avatar" src={Resume2}></img>
+            <Button className="resume-button" onClick={handleDownloadResume}>
+    Download my Resume
+</Button>
+
+            </Container>
+
+            <span className="social-icon">
+                <div>
+                  <a href="https://github.com/marphco" target="_blank">
+                    <img id="github" src={Github} alt="GitHub" />
+                  </a>
+                  <a href="https:/linkedin.com/in/marco-franco"  target="_blank">
+                    <img id="linkedin" src={Linkedin} alt="LinkedIn" />
+                  </a>
+                </div>
+              </span>
+
+        </Row>
+    )
+  }
+
+export default Resume
